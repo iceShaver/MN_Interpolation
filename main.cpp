45ee8da256a3567eb664dc8daed28beff624065c
iceShaver <kamil-krolikowski@outlook.com>
2018-06-01 12:59:13 +0200

    fixed lagrange

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iterator>
 #include <tuple>
 #include <future>
+#include <sstream>
 #include "string_tools.hh"
 #include "matrix.hh"
 #include "iostream_tools.hh"
@@ -30,32 +31,40 @@ auto make_lagrange(double const X, std::vector<Point> const &points) {
 
 
 int main() {
-    // read data
-    auto data = std::vector<Point>();
+    // read input
+    auto input = std::vector<Point>();
     {
         auto file = std::ifstream("data1.txt");
         std::transform(std::istream_iterator<std::string>(file),
                        std::istream_iterator<std::string>(),
-                       std::back_inserter(data),
+                       std::back_inserter(input),
                        [](auto const &line) {
                            auto const slices = split(line, ',');
                            return Point(std::stod(slices[0]), std::stod(slices[1]));
                        });
     }
-    // parallel lagrange and save data
+    // parallel lagrange and save input
+    // choose points with specified step
+    auto data = std::vector<Point>();
+    auto step = 20;
+    data.reserve(input.size()/step);
+    std::for_each(input.begin(), input.end(), [&, i=0] (auto x) mutable{ if(i%step == 0) {data.push_back(x);} ++i;});
     {
-        auto outFile = std::ofstream("output.txt");
+        auto interpolatedOutFile = std::ofstream("inter_out.txt");
+        auto dataOutFile = std::ofstream("data_out.txt");
+        for(auto[x,y]:data){dataOutFile << x << ',' << y << std::endl;}
         auto futures = std::vector<std::future<Point>>();
         futures.reserve(static_cast<unsigned long long>(data.back().first + 1));
-        // find values of subsequent points in input data range, step: 1 meter
+        // find values of subsequent points in input input range, step: 1 meter
         auto step = 1.0;
         for (auto x = data[0].first; x < data.back().first; x += step) {
             futures.push_back(std::async(std::launch::async, make_lagrange, x, data));
         }
         for (auto &future : futures) {
             auto[x, y] = future.get();
-            outFile << x << ',' << y << std::endl;
+            interpolatedOutFile << x << ',' << y << std::endl;
         }
+
     }
     return 0;
 }
--- make_plot.py
+++ make_plot.py
@@ -2,7 +2,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y'])
-plt.scatter(data['x'], data['y'], color='r', label='label', s=0.1)
+data1 = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y'])
+data2 = np.genfromtxt(sys.argv[2], delimiter=',', names=['x', 'y'])
+plt.scatter(data1['x'], data1['y'], color='r', s=0.1)
+plt.scatter(data2['x'], data2['y'], color='b', s=10.0)
 plt.axis([0, 3600, 0, 100])
 plt.show()
